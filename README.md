eda123c5b21cc002d5f51e55cb9b4c6daebfd74a
mark-pherz <marchello.den88@gmail.com>
2024-01-29 10:16:13 +0800

    a simple version with only one round

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # odin-paper-scissors-rock
+
+This is my first working JS code.
+I pushed the first code that worked on screen as expected.
+I deliberately didn't make any efforts so make it look better,
+be more efficient, more readable whatsoever. I want to keep it
+as bad as it could be so I can come back here later and laugh.
--- script.js
+++ script.js
@@ -9,63 +9,55 @@ function getComputerChoice() {
   }
 }
 
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+const rockBtn = document.getElementById("rock");
+
+const table = document.getElementById("table");
+
+let playerSelection;
+
+paperBtn.addEventListener("click", () => {
+  playerSelection = "paper";
+  table.innerText = playRound(playerSelection, getComputerChoice());
+});
+
+scissorsBtn.addEventListener("click", () => {
+  playerSelection = "scissors";
+  table.innerText = playRound(playerSelection, getComputerChoice());
+});
+
+rockBtn.addEventListener("click", () => {
+  playerSelection = "rock";
+  table.innerText = playRound(playerSelection, getComputerChoice());
+});
+
+
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
   if (playerSelection === computerSelection) {
-    return "It's a tie";
+    return `You both chose ${playerSelection}, it's a tie.`;
   }
-  if (playerSelection === "rock" && computerSelection === "paper") {
-    return "Computer won! Paper beats rock.";
-  }
-  if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You won! Rock beats scissors.";
-  }
-  if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You won! Paper beats rock.";
-  }
-  if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "Computer won! Scissors beat paper.";
-  }
-  if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "Computer won! Rock beats scissors.";
-  }
-  if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You Win! Scissors beat paper.";
-  }
-  return "please enter either paper, scissors or rock";
-}
-
-// const playerSelection = prompt("Choose paper scissors or rock", "scissors");
-// const computerSelection = getComputerChoice();
-
-// // console.log(`Your choice is ${playerSelection.toLowerCase()}`);
-// console.log(`Computer choice is ${computerSelection}`);
-// console.log(playRound(playerSelection, computerSelection));
-
-function game() {
-  let gameCount = 0;
-  let playerScore = 0;
-  let computerScore = 0;
-  while (gameCount < 5) {
-    const playerSelection = prompt("Choose paper scissors or rock", "scissors");
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    if (playRound(playerSelection, computerSelection).charAt(0) === "Y") {
-      playerScore = playerScore + 1;
-    }
-    if (playRound(playerSelection, computerSelection).charAt(0) === "C") {
-      computerScore = computerScore + 1;
-    }
-    gameCount++;
-  }
-
-  if (playerScore > computerScore) {
-    console.log("You won the tournament");
-  } else if (computerScore > playerScore) {
-    console.log("Computer won the tournament");
-  } else {
-    console.log("Tournament is a scam.");
+  switch (playerSelection) {
+    case "paper":
+      switch (computerSelection) {
+        case "scissors":
+          return "Computer won, scissors cut the paper.";
+        case "rock":
+          return "You win, paper covers the rock.";
+      }
+    case "scissors":
+      switch (computerSelection) {
+        case "rock":
+          return "Computer won, rock breaks scissors.";
+        case "paper":
+          return "You win, scissors cut paper;";
+      }
+    case "rock":
+      switch (computerSelection) {
+        case "paper":
+          return "Computer won, paper covers the rock";
+        case "scissors":
+          return "You win, rock breaks scissors.";
+      }
   }
 }
-
-game();
