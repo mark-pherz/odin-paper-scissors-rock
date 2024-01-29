844e4d09122a383767155507f89a6e35aa98e113
mark-pherz <marchello.den88@gmail.com>
2024-01-29 10:51:43 +0800

    a version that supports multiple rounds and shows current score

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-function getComputerChoice() {
-  let random = Math.floor(Math.random() * 3) + 1;
-  if (random === 1) {
-    return "scissors";
-  } else if (random === 2) {
-    return "paper";
-  } else {
-    return "rock";
-  }
-}
-
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
 const rockBtn = document.getElementById("rock");
-
 const table = document.getElementById("table");
 
+let playerScore = 0;
+let computerScore = 0;
 let playerSelection;
 
 paperBtn.addEventListener("click", () => {
   playerSelection = "paper";
   table.innerText = playRound(playerSelection, getComputerChoice());
+  document.getElementById("playerScr").innerText = playerScore;
+  document.getElementById("computerScr").innerText = computerScore;
 });
 
 scissorsBtn.addEventListener("click", () => {
   playerSelection = "scissors";
   table.innerText = playRound(playerSelection, getComputerChoice());
+  document.getElementById("playerScr").innerText = playerScore;
+  document.getElementById("computerScr").innerText = computerScore;
 });
 
 rockBtn.addEventListener("click", () => {
   playerSelection = "rock";
   table.innerText = playRound(playerSelection, getComputerChoice());
+  document.getElementById("playerScr").innerText = playerScore;
+  document.getElementById("computerScr").innerText = computerScore;
 });
-
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
@@ -41,23 +36,40 @@ function playRound(playerSelection, computerSelection) {
     case "paper":
       switch (computerSelection) {
         case "scissors":
+          computerScore++;
           return "Computer won, scissors cut the paper.";
         case "rock":
+          playerScore++;
           return "You win, paper covers the rock.";
       }
     case "scissors":
       switch (computerSelection) {
         case "rock":
+          computerScore++;
           return "Computer won, rock breaks scissors.";
         case "paper":
-          return "You win, scissors cut paper;";
+          playerScore++;
+          return "You win, scissors cut paper.";
       }
     case "rock":
       switch (computerSelection) {
         case "paper":
-          return "Computer won, paper covers the rock";
+          computerScore++;
+          return "Computer won, paper covers the rock.";
         case "scissors":
+          playerScore++;
           return "You win, rock breaks scissors.";
       }
+  }
+}
+
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3) + 1;
+  if (random === 1) {
+    return "scissors";
+  } else if (random === 2) {
+    return "paper";
+  } else {
+    return "rock";
   }
 }
